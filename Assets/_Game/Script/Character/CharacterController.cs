7e8fd2433ec12677232bf66f3d69d9bc047775e4
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2023-03-02 03:23:44 +0700

    Fix spawn bullets

--- Assets/_Game/Script/Character/CharacterController.cs
+++ Assets/_Game/Script/Character/CharacterController.cs
@@ -3,50 +3,42 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Reference to the bullet prefab and spawn point transform
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-
-    // Speed of the bullet
     public float bulletSpeed = 20f;
-
-    // Flag to check if the player can shoot
     private bool canShoot = true;
 
-    // FixedUpdate is called once per physics update
     private void FixedUpdate()
     {
-        // Check if the character is not moving and can shoot
-        if (GetComponent<Rigidbody>().velocity.magnitude <= 0f && canShoot)
+        // Check if the character is moving
+        if (GetComponent<Rigidbody>().velocity.magnitude > 0f)
         {
-            // Check for bots within the range
+            canShoot = true;
+        }
+        else if (canShoot)
+        {
             Collider[] botColliders = Physics.OverlapSphere(transform.position, 2.56f, LayerMask.GetMask("Bot"));
-
-            // If a bot is found, shoot at it
             if (botColliders.Length > 0)
             {
+                // Shoot at the first Bot collider found
                 Vector3 botPosition = botColliders[0].transform.position;
+                Vector3 shootDirection = (botPosition - transform.position).normalized;
                 bulletSpawn.LookAt(botPosition);
                 Shoot();
             }
-
-            // Mark that the player has shot and cannot shoot again until they move
             canShoot = false;
         }
     }
 
-    // Coroutine to disable bullets after a certain time
     private IEnumerator DisableBullet(GameObject bullet)
     {
         yield return new WaitForSeconds(2f);
         bullet.SetActive(false);
     }
 
-    // Method to shoot a bullet
     private void Shoot()
     {
         GameObject bullet = ObjectPooling.instance.GetPoolObject();
-
         if (bullet != null)
         {
             bullet.transform.position = bulletSpawn.position;
@@ -54,7 +46,6 @@ public class CharacterController : MonoBehaviour
             bullet.SetActive(true);
             bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
 
-            // Disable the bullet after a certain time
             StartCoroutine(DisableBullet(bullet));
         }
         else
