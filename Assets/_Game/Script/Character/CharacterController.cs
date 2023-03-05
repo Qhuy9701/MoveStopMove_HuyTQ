06ac602af5e119708dec5d9cad8ed113c62b979b
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2023-03-05 12:54:27 +0700

    Multi Object Pool

--- Assets/_Game/Script/Character/CharacterController.cs
+++ Assets/_Game/Script/Character/CharacterController.cs
@@ -3,54 +3,69 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public Transform bulletSpawn;
-    public float bulletSpeed = 20f;
-    private bool canShoot = true;
+public GameObject bulletPrefab;
+public Transform bulletSpawn;
+public float bulletSpeed = 20f;
+private bool canShoot = true;
+private ObjectPool objectPool;
+ private void Awake()
+{
+    objectPool = FindObjectOfType<ObjectPool>();
+}
 
-    private void FixedUpdate()
+private void FixedUpdate()
+{
+    // Check if the character is moving
+    if (GetComponent<Rigidbody>().velocity.magnitude > 0f)
     {
-        // Check if the character is moving
-        if (GetComponent<Rigidbody>().velocity.magnitude > 0f)
+        canShoot = true;
+    }
+    else if (canShoot)
+    {
+        Collider[] botColliders = Physics.OverlapSphere(transform.position, 2.56f, LayerMask.GetMask("Bot"));
+        if (botColliders.Length > 0)
         {
-            canShoot = true;
+            // Shoot at the first Bot collider found
+            Vector3 botPosition = botColliders[0].transform.position;
+            Vector3 shootDirection = (botPosition - transform.position).normalized;
+            bulletSpawn.LookAt(botPosition);
+            Shoot();
         }
-        else if (canShoot)
+        canShoot = false;
+    }
+}
+
+private IEnumerator DisableBullet(GameObject bullet)
+{
+    yield return new WaitForSeconds(2f);
+    bullet.SetActive(false);
+}
+
+private void Shoot()
+{
+    if (objectPool == null)
+    {
+        objectPool = FindObjectOfType<ObjectPool>();
+        if (objectPool == null)
         {
-            Collider[] botColliders = Physics.OverlapSphere(transform.position, 2.56f, LayerMask.GetMask("Bot"));
-            if (botColliders.Length > 0)
-            {
-                // Shoot at the first Bot collider found
-                Vector3 botPosition = botColliders[0].transform.position;
-                Vector3 shootDirection = (botPosition - transform.position).normalized;
-                bulletSpawn.LookAt(botPosition);
-                Shoot();
-            }
-            canShoot = false;
+            Debug.LogError("ObjectPool instance not found.");
+            return;
         }
     }
 
-    private IEnumerator DisableBullet(GameObject bullet)
+    GameObject bullet = objectPool.SpawnFromPool("Bullet", bulletSpawn.position, bulletSpawn.rotation);
+    if (bullet != null)
     {
-        yield return new WaitForSeconds(2f);
-        bullet.SetActive(false);
-    }
+        bullet.transform.position = bulletSpawn.position;
+        bullet.transform.rotation = bulletSpawn.rotation;
+        bullet.SetActive(true);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
 
-    private void Shoot()
+        StartCoroutine(DisableBullet(bullet));
+    }
+    else
     {
-        GameObject bullet = BulletPool.instance.GetPoolObject();
-        if (bullet != null)
-        {
-            bullet.transform.position = bulletSpawn.position;
-            bullet.transform.rotation = bulletSpawn.rotation;
-            bullet.SetActive(true);
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-
-            StartCoroutine(DisableBullet(bullet));
-        }
-        else
-        {
-            Debug.Log("No available bullets in pool.");
-        }
+        Debug.Log("No available bullets in pool.");
     }
+}
 }
