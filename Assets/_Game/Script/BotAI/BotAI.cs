be69d1bf38cfdfeb9ed4ad23ce2a2c999634bc33
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2023-03-07 04:06:42 +0700

    up

--- Assets/_Game/Script/BotAI/BotAI.cs
+++ Assets/_Game/Script/BotAI/BotAI.cs
@@ -1,112 +1,111 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BotAI : CharacterController
 {
-    private GameObject currentTarget;
-    private NavMeshAgent agent;
-    private bool isAttacking;
-    private bool canAttack;
+private GameObject currentTarget;
+private NavMeshAgent agent;
+private bool isAttacking;
+private bool canAttack;
+private void Awake()
+{
+    agent = GetComponent<NavMeshAgent>();
+}
 
-    private void Awake()
+private void Start()
+{
+    currentTarget = GetNewTarget();
+    agent.SetDestination(currentTarget.transform.position);
+}
+
+private void Update()
+{
+    if (!isDead)
     {
-        agent = GetComponent<NavMeshAgent>();
-    }
+        if (currentTarget == null)
+        {
+            currentTarget = GetNewTarget();
+            agent.SetDestination(currentTarget.transform.position);
+        }
 
-    private void Start()
+        if (!isAttacking && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            canAttack = true;
+            agent.isStopped = true;
+            agent.velocity = Vector3.zero;
+
+            if (currentTarget.CompareTag("Character"))
+            {
+                BotAI targetBot = currentTarget.GetComponent<BotAI>();
+                if (targetBot != null && targetBot.isDead)
+                {
+                    currentTarget = GetNewTarget();
+                    agent.SetDestination(currentTarget.transform.position);
+                }
+            }
+
+            if (canAttack)
+            {
+                isAttacking = true;
+                canAttack = false;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+}
+
+private IEnumerator Attack()
+{
+    yield return new WaitForSeconds(2f);
+    currentTarget = GetNewTarget();
+
+    if (currentTarget.CompareTag("Character"))
+    {
+        BotAI targetBot = currentTarget.GetComponent<BotAI>();
+        if (targetBot != null && !targetBot.isDead)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(currentTarget.transform.position);
+            isAttacking = false;
+        }
+        else
+        {
+            currentTarget = GetNewTarget();
+            agent.SetDestination(currentTarget.transform.position);
+            isAttacking = false;
+        }
+    }
+    else
     {
         currentTarget = GetNewTarget();
         agent.SetDestination(currentTarget.transform.position);
+        isAttacking = false;
     }
+}
 
-    private void Update()
+private GameObject GetNewTarget()
+{
+    List<GameObject> targets = GameManager.Instance.GetCharacters();
+
+    // Remove this bot from the list of targets
+    targets.Remove(gameObject);
+
+    GameObject newTarget = targets[Random.Range(0, targets.Count)];
+
+    if (newTarget == gameObject)
     {
-        if (!isDead)
-        {
-            if (currentTarget == null)
-            {
-                currentTarget = GetNewTarget();
-                agent.SetDestination(currentTarget.transform.position);
-            }
-
-            if (!isAttacking && agent.remainingDistance <= agent.stoppingDistance)
-            {
-                canAttack = true;
-                agent.isStopped = true;
-                agent.velocity = Vector3.zero;
-
-                if (currentTarget.CompareTag("Character"))
-                {
-                    BotAI targetBot = currentTarget.GetComponent<BotAI>();
-                    if (targetBot != null && targetBot.isDead)
-                    {
-                        currentTarget = GetNewTarget();
-                        agent.SetDestination(currentTarget.transform.position);
-                    }
-                }
-
-                if (canAttack)
-                {
-                    isAttacking = true;
-                    canAttack = false;
-                    //StartCoroutine(Attack());
-                }
-            }
-        }
+        return GetNewTarget();
     }
 
-    // private IEnumerator Attack()
-    // {
-    //     yield return new WaitForSeconds(2f);
-    //     currentTarget = GetNewTarget();
+    return newTarget;
+}
 
-    //     if (currentTarget.CompareTag("Character"))
-    //     {
-    //         BotAI targetBot = currentTarget.GetComponent<BotAI>();
-    //         if (targetBot != null && !targetBot.isDead)
-    //         {
-    //             agent.isStopped = false;
-    //             agent.SetDestination(currentTarget.transform.position);
-    //             isAttacking = false;
-    //         }
-    //         else
-    //         {
-    //             currentTarget = GetNewTarget();
-    //             agent.SetDestination(currentTarget.transform.position);
-    //             isAttacking = false;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         currentTarget = GetNewTarget();
-    //         agent.SetDestination(currentTarget.transform.position);
-    //         isAttacking = false;
-    //     }
-    // }
-
-    private GameObject GetNewTarget()
-    {
-        List<GameObject> targets = GameManager.Instance.GetCharacters();
-
-        // Remove this bot from the list of targets
-        targets.Remove(gameObject);
-
-        GameObject newTarget = targets[Random.Range(0, targets.Count)];
-
-        if (newTarget == gameObject)
-        {
-            return GetNewTarget();
-        }
-
-        return newTarget;
-    }
-
-    public  void TakeDamage()
-    {
-        isDead = true;
-        agent.isStopped = true;
-        agent.velocity = Vector3.zero;
-    }
+public void TakeDamage()
+{
+    isDead = true;
+    agent.isStopped = true;
+    agent.velocity = Vector3.zero;
+}
 }
--- Assets/_Game/Script/BotAI/CharacterSpawner.cs
+++ Assets/_Game/Script/BotAI/CharacterSpawner.cs
@@ -1,101 +1,98 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System.Collections;
 
 public class CharacterSpawner : MonoBehaviour
 {
-[SerializeField] private float minSpawnDistance = 5f;
-[SerializeField] private float maxSpawnDistance = 15f;
-[SerializeField] private int spawnBatchSize = 10;
-[SerializeField] private float respawnDelay = 2f;
-public List<GameObject> spawnedBots = new List<GameObject>();
-GameManager gameManager;
+    [SerializeField] private float minSpawnDistance = 5f;
+    [SerializeField] private float maxSpawnDistance = 15f;
+    [SerializeField] private int spawnBatchSize = 10;   
+    public List<GameObject> spawnedBots = new List<GameObject>();
+    GameManager gameManager;
 
-private void Awake()
-{
-    gameManager = GameManager.Instance;
-}
-
-private void Start()
-{
-    SpawnPlayer();
-    SpawnBots();
-}
-
-public void SpawnPlayer()
-{
-    GameObject player = ObjectPool.Instance.SpawnFromPool("Player", GetRandomSpawnPosition(), Quaternion.identity);
-    if (player == null)
+    private void Awake()
     {
-        Debug.LogWarning("Không thể tạo player mới vì player pool đã hết");
-        return;
+        gameManager = GameManager.Instance;
     }
-    player.transform.position = GetRandomSpawnPosition();
-    player.SetActive(true);
-    player.name = "Player";
-    spawnedBots.Add(player);
-    gameManager.AddCharacter(player);
-}
 
-public void SpawnBots()
-{
-    for (int i = 0; i < spawnBatchSize; i++)
+    private void Start()
     {
-        GameObject newBot = ObjectPool.Instance.SpawnFromPool("Bot", GetRandomSpawnPosition(), Quaternion.identity);
-        if (newBot == null)
+        SpawnPlayer();
+        SpawnBots();
+    }
+
+    public void SpawnPlayer()
+    {
+        GameObject player = ObjectPool.Instance.SpawnFromPool("Player", GetRandomSpawnPosition(), Quaternion.identity);
+        if (player == null)
         {
-            Debug.LogWarning("Không thể tạo bot mới vì bot pool đã hết");
+            Debug.LogWarning("Không thể tạo player mới vì player pool đã hết");
             return;
         }
-        newBot.transform.position = GetRandomSpawnPosition();
-        newBot.SetActive(true);
-        newBot.name = "Bot " + i;
-        spawnedBots.Add(newBot);
-        gameManager.AddCharacter(newBot);
+        player.transform.position = GetRandomSpawnPosition();
+        player.SetActive(true);
+        player.name = "Player";
+        spawnedBots.Add(player);
+        gameManager.AddCharacter(player);
     }
-}
-
-public Vector3 GetRandomSpawnPosition()
-{
-    Vector3 playerPosition = transform.position;
-    Vector3 spawnDirection = Random.onUnitSphere;
-    spawnDirection.y = 0f;
-    float spawnDistance = Random.Range(minSpawnDistance, maxSpawnDistance);
-    return playerPosition + spawnDirection.normalized * spawnDistance;
-}
-
-private void OnCollisionEnter(Collision collision)
-{
-    GameObject collidedObject = collision.gameObject;
-    if (collidedObject.CompareTag("Bot"))
+    public void SpawnBots()
     {
-        RemoveBot(collidedObject);
+        for (int i = 0; i < spawnBatchSize; i++)
+        {   
+            //multiobject pool
+            GameObject newBot = ObjectPool.Instance.SpawnFromPool("Bot", GetRandomSpawnPosition(), Quaternion.identity);
+            if (newBot == null)
+            {
+                Debug.LogWarning("Không thể tạo bot mới vì bot pool đã hết");
+                return;
+            }
+            newBot.transform.position = GetRandomSpawnPosition();
+            newBot.SetActive(true);
+            newBot.name = "Bot " + i;
+            spawnedBots.Add(newBot);
+            gameManager.AddCharacter(newBot);
+        }
     }
-}
 
-public void RemoveBot(GameObject bot)
-{
-    spawnedBots.Remove(bot);
-    gameManager.RemoveCharacter(bot);
-    bot.SetActive(false);
-    StartCoroutine(ReactivateBotAfterDelay(bot, respawnDelay));
-}
+    private Vector3 GetRandomSpawnPosition()
+    {
+        // Nhận một vị trí ngẫu nhiên trên bản đồ, cách xa người chơi
+        Vector3 playerPosition = transform.position;
+        Vector3 spawnDirection = Random.onUnitSphere;
+        spawnDirection.y = 0f;
+        float spawnDistance = Random.Range(minSpawnDistance, maxSpawnDistance);
+        return playerPosition + spawnDirection.normalized * spawnDistance;
+    }
 
-private IEnumerator ReactivateBotAfterDelay(GameObject bot, float delay)
-{
-    yield return new WaitForSeconds(delay);
-    
-    bot.transform.position = GetRandomSpawnPosition();
-    bot.SetActive(true);
-    spawnedBots.Add(bot);
-    gameManager.AddCharacter(bot);
-}
+    public void RemoveBot(GameObject bot)
+    {
+        // Xóa bot khỏi danh sách các bot đã được tạo ra
+        spawnedBots.Remove(bot);
+        gameManager.RemoveCharacter(bot);   
 
-private void OnDrawGizmosSelected()
-{
-    Gizmos.color = Color.yellow;
-    Gizmos.DrawWireSphere(transform.position, maxSpawnDistance);
-}
+        // Trả lại bot vào pool
+        bot.SetActive(false);
+        ObjectPool.Instance.ReturnToPool("Enemy", bot);
 
-//take damagea
+        // Kiểm tra xem pool có đủ bot không, nếu không thì tạo mới
+        if (spawnedBots.Count < spawnBatchSize)
+        {
+            GameObject newBot = ObjectPool.Instance.SpawnFromPool("Enemy", GetRandomSpawnPosition(), Quaternion.identity);
+            if (newBot == null)
+            {
+                Debug.LogWarning("Không thể tạo bot mới vì bot pool đã hết");
+                return;
+            }
+            newBot.transform.position = GetRandomSpawnPosition();
+            newBot.SetActive(true);
+            spawnedBots.Add(newBot);
+            gameManager.AddCharacter(newBot);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Vẽ quả cầu xung quanh người chơi để hiển thị khoảng cách sinh bot
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxSpawnDistance);
+    }
 }
