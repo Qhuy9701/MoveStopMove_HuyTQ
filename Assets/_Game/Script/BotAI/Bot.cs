0a60e5df4070f986674079428ac1e932c8fbbf4b
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2023-03-17 03:30:19 +0700

    Push AI

--- Assets/_Game/Script/BotAI/Bot.cs
+++ Assets/_Game/Script/BotAI/Bot.cs
@@ -1,73 +1,103 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.TextCore.Text;
 
-public class Bot : CharacterController
+public class Bot : Character
 {
-    private NavMeshAgent agent;
-    private List<GameObject> characters = new List<GameObject>();
-    private CharacterSpawner characterSpawner;
+    [SerializeField] private GameObject aimed;
 
-    private void Awake()
+    [SerializeField] Character currentTarget;
+    public Character CurrentTarget { get => currentTarget; set => currentTarget = value; }
+
+    private NavMeshAgent navMeshAgent;
+    private IState currentState;
+
+
+    protected override void Awake()
     {
         base.Awake();
-        characterSpawner = FindObjectOfType<CharacterSpawner>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = 3;
+        ChangeState(new PatrolState());
+    }
+
+    public override void OnInit()
+    {
+        base.OnInit();
+        currentTarget = null;
+        aimed.SetActive(true);
+        navMeshAgent.enabled = true;
+    }
+
+    public override void OnDespawn()
+    {
+        StopMoving();
+        base.OnDespawn();
     }
 
     private void Start()
     {
-        if (characterSpawner != null)
-        {
-            characters = characterSpawner.GetCharacters();
-            if (characters.Count > 0)
-            {
-                _currentTarget = characters[Random.Range(0, characters.Count)].transform;
-            }
-            else
-            {
-                Debug.LogError("No characters found in list!");
-                return;
-            }
-        }
-        else
-        {
-            Debug.LogError("CharacterSpawner not found!");
-            return;
-        }
+        navMeshAgent.SetDestination(CurrentTarget.transform.position);
     }
 
     private void Update()
     {
-        // Kiểm tra khoảng cách giữa Bot và mục tiêu hiện tại
-        float distance = Vector3.Distance(transform.position, _currentTarget.position);
-        if (distance <= _attackRange)
-        {
-            // Dừng lại nếu mục tiêu nằm trong range bắn
-            agent.isStopped = true;
-            // Random lại target sau 0.5 giây
-            StartCoroutine(RandomTargetAfterDelay(0.5f));
-        }
-        else
-        {
-            // Tiếp tục di chuyển tới target
-            agent.isStopped = false;
-            agent.SetDestination(_currentTarget.position);
-        }
+        currentState?.OnExecute(this);
     }
 
-    private IEnumerator RandomTargetAfterDelay(float delay)
+
+    public void StopMoving()
     {
-        yield return new WaitForSeconds(delay);
-        // Random lại target mới
-        if (characters.Count > 0)
+        navMeshAgent.velocity = Vector3.zero;
+        navMeshAgent.isStopped = true;
+    }
+    public void StartMoving()
+    {
+        navMeshAgent.isStopped = false;
+    }
+
+    public void ShowAim()
+    {
+        aimed.SetActive(true);
+    }
+
+    public void HideAim()
+    {
+        aimed.SetActive(false);
+    }
+
+    public void MoveToTarget(Character target)
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+
+    public void ChangeState(IState newState)
+    {
+        currentState?.OnExit(this);
+        currentState = newState;
+        currentState?.OnEnter(this);
+    }
+
+    public override void CheckTargetNearest()
+    {
+        float minDistance = float.MaxValue;
+        Character currentTargetTmp = null;
+        if (CharactersTargeted.Count == 0)
         {
-            _currentTarget = characters[Random.Range(0, characters.Count)].transform;
+            TargetNearest = null;
         }
-        else
+
+        for (int i = 0; i < CharactersTargeted.Count; i++)
         {
-            Debug.LogError("No characters found in list!");
+            float distance = Vector3.Distance(CharactersTargeted[i].transform.position, transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                currentTargetTmp = CharactersTargeted[i];
+            }
         }
+        TargetNearest = currentTargetTmp;
     }
 }
